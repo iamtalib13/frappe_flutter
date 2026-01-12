fe710a4bc102e9e6d92867a4676dcbe6969abb60
iamfaijank <iamfaijank@gmail.com>
2026-01-12 13:46:54 +0530

    feat :add name and logout button of app

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,14 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:get/get.dart';
+import 'login_page.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomePage extends StatefulWidget {
+  final String fullName;
+  final String email;
+
+  const HomePage({
+    super.key,
+    required this.fullName,
+    required this.email,
+  });
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final _secureStorage = const FlutterSecureStorage();
+
+  Future<void> _logout() async {
+    await _secureStorage.delete(key: 'sid');
+    Get.offAll(() => const LoginPage());
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home'),
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(widget.fullName),
+            Text(
+              widget.email,
+              style: const TextStyle(fontSize: 12.0),
+            ),
+          ],
+        ),
         backgroundColor: const Color(0xFF006767),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: _logout,
+          ),
+        ],
       ),
       body: const Center(
         child: Text(
